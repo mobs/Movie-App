9e1a60e3442c0b80c40f527363e316a291670f1c
Mobashir Alam <mobashira3@gmail.com>
2022-12-28 19:50:38 +0530

    added OMDB api and context

--- src/components/App.js
+++ src/components/App.js
@@ -1,79 +1,104 @@
 import React from 'react';
-import { data } from '../data';
-import Navbar from './Navbar'; 
+import Navbar from './Navbar';
 import MovieCard from './MovieCard';
 import { addMovies, setShowFavourites } from '../actions';
+import { data as moviesList } from '../data';
+import { StoreContext } from '../index';
 
 // converting this functional comp to class comp as we now want to take data from a DB and we need componentDidMount()
-
 class App extends React.Component {
-  componentDidMount(){
-    const {store} = this.props;
-    store.subscribe(() => {
-      console.log('UPDATED');
-      this.forceUpdate(); // should be avoided(here for simplicty only)
-    });
-    //make api call
-    //dispatch action
-    /*
-    store.dispatch({
-      type: 'ADD_MOVIES',
-      movies: data
-    });
-    */
-    store.dispatch(addMovies(data));
+  componentDidMount() {
+    // const {store} = this.props;
+    // store.subscribe(() => {
+    //   console.log('UPDATED');
+    //   this.forceUpdate(); // should be avoided(here for simplicty only)
+    // });
+    // //make api call
+    // //dispatch action
+    // /*
+    // store.dispatch({
+    //   type: 'ADD_MOVIES',
+    //   movies: data
+    // });
+    // */
+    // store.dispatch(addMovies(data));
 
-    console.log('STATE',this.props.store.getState());
+    // console.log('STATE',this.props.store.getState());
+
+    this.props.store.subscribe(() => this.forceUpdate());
+    this.props.store.dispatch(addMovies(moviesList));
   }
-  
-  isMovieFavourite = (movie) => {
+
+  isMovieInFavourites = (movie) => {
     const { movies } = this.props.store.getState();
 
     const index = movies.favourites.indexOf(movie);
-    if(index !== -1){
+    if (index !== -1) {
       //found the movie
       return true;
     }
-    return false;
-  }
 
-  onChangeTab = (val) => {
+    return false;
+  };
+
+  changeTab = (val) => {
     this.props.store.dispatch(setShowFavourites(val));
-  }
+  };
   render() {
     // initially we were getting an array and mappin over it but now we get an object so we should use object destructuring
     // const movies = this.props.store.getState();
-
-    const { movies } = this.props.store.getState(); // {movies: {}, search: {}}
-    const { list, favourites, showFavourites } = movies;
-    
+    const { movies, search } = this.props.store.getState(); // will return { movies: {}, search: []}
+    console.log('movies', movies);
+    const { list, showFavourites = [], favourites = [] } = movies;
     console.log('RENDER',this.props.store.getState());
-
     const displayMovies = showFavourites ? favourites : list;
+
     return (
       <div className="App">
-        <Navbar />
+        <Navbar search={search} />
         <div className="main">
           <div className="tabs">
-            <div className={`tab ${showFavourites ? '' : 'active-tabs'} `} onClick={() => this.onChangeTab(false)}>Movies</div>
-            <div className={`tab ${showFavourites ? 'active-tabs' : ''} `}  onClick={() => this.onChangeTab(true)}>Favourites</div>
+            <div
+              className={`tab ${showFavourites ? '' : 'active-tabs'}`}
+              onClick={() => this.changeTab(false)}
+            >
+              Movies
+            </div>
+            <div
+              className={`tab ${showFavourites ? 'active-tabs' : ''}`}
+              onClick={() => this.changeTab(true)}
+            >
+              Favourites
+            </div>
           </div>
 
-          <div className='list'>
-            {displayMovies.map((movie, index) => (
-              <MovieCard 
-                movie={movie} 
-                key={`movies-${index}`} 
-                dispatch={this.props.store.dispatch} 
-                isFavourite = {this.isMovieFavourite(movie)}
+          <div id="list">
+            {displayMovies.map((movie) => (
+              <MovieCard
+                movie={movie}
+                key={movie.imdbID}
+                dispatch={this.props.store.dispatch}
+                isFavourite={this.isMovieInFavourites(movie)}
               />
             ))}
+            {displayMovies.length === 0 ? (
+              <div className="no-movies">No movies to display! </div>
+            ) : null}
           </div>
-          {displayMovies.length === 0 ? <div className='no-movies'> No movies to display! </div> : null}
         </div>
       </div>
     );
   }
 }
 
-export default App;
+class AppWrapper extends React.Component {
+  render() {
+    return (
+      <StoreContext.Consumer>
+        {(store) => <App store={store} />}
+      </StoreContext.Consumer>
+    );
+  }
+}
+
+export default AppWrapper;
--- src/index.js
+++ src/index.js
@@ -1,28 +1,81 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { createStore } from 'redux';
+import React, { createContext } from 'react';
+import ReactDOM from 'react-dom';
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
 
 import App from './components/App';
-import './index.css'
-// import movies from './reducers';
 import rootReducer from './reducers';
+import './index.css';
 
+// const logger = function({ dispatch, getState }) {
+//   return function(next) {
+//     return function(action) {
+//       // my middlware
+//       console.log('ACTION', action);
+//       next(action);
+//     };
+//   };
+// };
+//function logger(obj, next, action)
+// logger(obj)(next)(action)  // internally it is called by react like this
+// const logger = function({ dispatch, getState }) {
+//   return function (next) {
+//     return function (action) {
+//       // middleware code here
+//       console.log('ACTION_TYPE = ',action.type);
+//       next(action);
+//     }
+//   }
+// }
 
-const store = createStore(rootReducer);
-console.log('store',store);
-// console.log('before state',store.getState());
+// another clenaer way of writing middlewares
 
+const logger = ({ dispatch, getState }) => (next) => (action) => {
+
+  // if(typeof action !== 'function'){
+    console.log('ACTION', action);
+    next(action);
+  // }
+};
+
+// thunk is not needed coz it is available inbuilt as react-thunk
+// const thunk = store => next => action => {
+//   if (typeof action === 'function') {
+//     return action(store.dispatch);
+//   }
+
+//   next(action);
+// };
+
+const store = createStore(rootReducer, applyMiddleware(logger, thunk));
+// console.log(store);
+console.log('state', store.getState());
+
+export const StoreContext = createContext();
+
+console.log('StoreContext', StoreContext);
+
+class Provider extends React.Component {
+  render() {
+    const { store } = this.props;
+    return (
+      <StoreContext.Provider value={store}>
+        {this.props.children}
+      </StoreContext.Provider>
+    );
+  }
+}
+
+// update store by dispatching actions
 // store.dispatch({
 //   type: 'ADD_MOVIES',
-//   movies: [{ name: 'Superman'}]
+//   movies: moviesList
 // });
+// console.log('state', store.getState());
 
-// console.log('after state',store.getState());
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App store={store}/>  {/*passing store as a prop here */}
-  </React.StrictMode>
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
 );
-
